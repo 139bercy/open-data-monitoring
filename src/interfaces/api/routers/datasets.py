0b4273098ea0c071eb3d7e73a8a7db3aebb78237
HitowaSan <alexandre.merre.pro@gmail.com>
2025-07-24 09:37:05 +0200

    feat(api): Adding API and stubs for each routers | Implementing first route corresponding to "app common get-publishers"

---
+++ src/interfaces/api/routers/datasets.py
@@ -0,0 +1,13 @@
+"""
+Router pour les endpoints datasets (STUB)  
+TODO: Implémenter les endpoints dataset
+"""
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/datasets", tags=["datasets"])
+
+# TODO: Implémenter les endpoints
+# - POST /datasets (add dataset)
+# - GET /datasets/publisher/{name} (get by publisher)
+# - GET /datasets/tests (get test datasets) 
---
+++ src/interfaces/api/routers/platforms.py
@@ -0,0 +1,9 @@
+"""
+Router pour les endpoints platforms (STUB)
+TODO: Implémenter les endpoints platform
+"""
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/platforms", tags=["platforms"])
+
