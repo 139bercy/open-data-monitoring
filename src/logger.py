382fe9c2c5d0e1366255fcb97c1f24bad9b72d63
tongyeouki <tongyeouki@gmail.com>
2026-02-06 14:30:35 +0100

    fix(logger): test logs should not be written in app log file

--- src/logger.py
+++ src/logger.py
@@ -10,11 +10,13 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)  # ou logging.INFO
 console_handler.setFormatter(formatter)
 
-log_file = "app.log"
-file_handler = RotatingFileHandler("app.log", maxBytes=1_000_000, backupCount=5)
-file_handler.setLevel(logging.WARN)
-file_handler.setFormatter(formatter)
+import os
 
 if not logger.hasHandlers():
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+    
+    if os.getenv("OPEN_DATA_MONITORING_ENV") != "TEST":
+        file_handler = RotatingFileHandler("app.log", maxBytes=1_000_000, backupCount=5)
+        file_handler.setLevel(logging.WARN)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
